3acb16e0d2b0b9ff6362ed7b18d2617a1d82fd74
jac <systems@finect.com>
2014-01-29 12:43:55 +0100

    config changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+config.frameworks -= [ :active_record, :active_support ] 
+
 # Initialize the Rails application.
 CampaignNameValidator::Application.initialize!
 
-config.frameworks -= [ :active_record ] 
--- test/controllers/validation_controller_test.rb
+++ test/controllers/validation_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class ValidationControllerTest < ActionController::TestCase
+  setup do
+    # TODO: consider to use fixtures
+    @right_campaign_name = 'interview'
+    @wrong_campaign_name = '¬½€ł'
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
-  test "should get validate" do
-    get :validate
+  test "should validate right campaign name" do
+    post :validate, :name => @right_campaign_name
     assert_response :success
   end
 
+    post :validate, :name => @wrong_campaign_name
+    assert_response :fail
+  end
+  
 end
