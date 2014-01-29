f48d2f6f985d5fcd82df272653c1fa0e5d139202
jac <systems@finect.com>
2014-01-29 11:31:40 +0100

    Validation message

--- app/controllers/validation_controller.rb
+++ app/controllers/validation_controller.rb
@@ -3,5 +3,14 @@ class ValidationController < ApplicationController
   end
 
   def validate
+  	campaign_name = params[:name]                                                                                                                                                                                                            
+    campaign = Campaign.new(campaign_name)                                                                                                                                                                                                   
+    if campaign.valid?                                                                                                                                                                                                                       
+      @result = "Valid Name"                                                                                                                                                                                                                 
+    else                                                                                                                                                                                                                                     
+      @result = "Invalid Name"                                                                                                                                                                                                               
+    end                                                                                                                                                                                                                                      
+
+    render :validate
   end
 end
