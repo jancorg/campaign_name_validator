a6a0314595cfabfe7e8e255001f7741426d7dfbc
jac <systems@finect.com>
2014-01-29 17:01:43 +0100

    Typo error removed

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,7 +1,6 @@
 class AsciiStringValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-  	Rails.logger = Logger.new(STDOUT)
-    Rails.logger = Log4r::Logger.new( "El valor de lo huevos es " + value)
+  	Rails.logger.debug("El valor de value es: " + value)
     #record.errors.add :name, "has non asscii chars" unless value.ascii_only?
     value.ascii_only?
   end
