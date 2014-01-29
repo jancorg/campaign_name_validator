e78f4779dc6bd77bdc8e46d92f1af8d9333b070f
jac <systems@finect.com>
2014-01-29 11:46:27 +0100

    Database disabled

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 CampaignNameValidator::Application.initialize!
+
+config.frameworks -= [ :active_record ] 
