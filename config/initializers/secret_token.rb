e35a9ab74f805e92a54fb91239f1ad44dbda0864
jac <systems@finect.com>
2014-01-29 11:18:10 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CampaignNameValidator::Application.config.secret_key_base = 'a5c1d2e49b6b92f8e6321ce8788fd3aa29a01857b768b159b1f25caca1420985d567a6a93700534fbb3faecf13424a630166e3039e847f45dcc0d35a62c76bbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CampaignNameValidator::Application.config.session_store :cookie_store, key: '_campaign_name_validator_session'
